db7408939cd63ac57513d19aab650dd2ff8ed5f6
dgermano17 <danilo.germano@etec.sp.gov.br>
2019-03-12 21:01:00 -0300

    inicial

---
+++ CalculadoraSimples/CalculadoraSimples/src/calculadorasimples/CalculadoraSimples.java
@@ -0,0 +1,40 @@
+
+package calculadorasimples;
+
+import javax.swing.*;
+
+public class CalculadoraSimples { 
+    static String aux;
+    static String opr;
+    static double num1, num2, resp = 0.0;
+    public static void main(String[] args) {
+        opr = JOptionPane.showInputDialog(null,"OPERAÇÕES: \n 1| SOMA \n 2| SUBTRAÇÃO \n 3| MULTIPLICAÇÃO \n 4| DIVISÃO \n \n Insira o código da operação desejada:");
+        aux = JOptionPane.showInputDialog(null,"Insira o primeiro valor:");
+        num1 = Double.parseDouble(aux);
+        aux = JOptionPane.showInputDialog(null,"Insira o segundo valor:");
+        num2 = Double.parseDouble(aux);
+        
+        //JOptionPane.showMessageDialog(null,"NUM1: "+num1+" NUM2: "+num2);
+        CalculadoraSimples.Resposta(num1,num2);
+    }
+    
+    
+    public static void Resposta(double num1,double num2){
+        if (opr == "1"){
+            resp = Operacoes.somaValor(num1, num2);
+        }else if (opr == "2"){
+            resp = Operacoes.subtValor(num1, num2);
+        }else if (opr == "3"){
+            resp = Operacoes.multValor(num1, num2);
+        }else if (opr == "4"){
+            resp = Operacoes.divValor(num1, num2);
+        }
+        JOptionPane.showMessageDialog(null, "Resultado: "+resp);
+    }
+
+    
+
+}
+        
+      
+
---
+++ CalculadoraSimples/CalculadoraSimples/src/calculadorasimples/Operacoes.java
@@ -0,0 +1,20 @@
+
+package calculadorasimples;
+
+public class Operacoes {
+    public static double somaValor(double num1, double num2){
+        return (num1+num2);
+    }
+    
+    public static double subtValor(double num1, double num2){
+        return (num1-num2);
+    }
+    
+    public static double multValor(double num1, double num2){
+        return (num1*num2);
+    }
+    
+    public static double divValor(double num1, double num2){
+        return (num1/num2);
+    }
+}
